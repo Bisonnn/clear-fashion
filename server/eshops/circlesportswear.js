dd64338c52dc54df17705fffaa5cd0c7d197f52f
Bisonnn <elias.bouasria@edu.devinci.fr>
2023-03-21 00:15:21 +0100

    ws3 finihed

---
+++ server/eshops/circlesportswear.js
@@ -0,0 +1,57 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+/**
+ * Parse webpage e-shop
+ * @param  {String} data - html response
+ * @return {Array} products
+ * */
+
+const parse = data => {
+    const $ = cheerio.load(data);
+    
+    //I want to scrap the product name and price on circlesportswear.com
+   
+    return $('.product-grid .grid__item')
+        .map((i, element) => {
+        const name = $(element)
+            .find('.card__heading.h5 a')
+            .text()
+            .trim()
+            .replace(/\s/g, ' ');
+        const price = parseInt(
+            $(element)
+            .find('.price-item--regular .money')
+            .text()
+            .replace(/\D/g, '')
+        );
+        return {name, price};
+        })
+        .get();
+    }
+
+
+/**
+ * Scrape all the products for a given url page
+ * @param  {[type]}  url
+ * @return {Array|null}
+ * */
+module.exports.scrape = async url => {
+    try{
+        const response = await fetch(url);
+        
+        if(response.ok){
+            const body = await response.text();
+            
+            return parse(body);
+        }
+        
+        console.error(response);
+        
+        return null;
+    }catch(error){
+        console.error(error);
+        return null;
+
+    }
+}
--- server/sandbox.js
+++ server/sandbox.js
@@ -1,7 +1,13 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./eshops/dedicatedbrand');
+const montlimar = require('./eshops/montlimart.js');
+const circlesports = require('./eshops/circlesportswear.js');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+const fs = require('fs');
+
+console.log(process.argv);
+
+async function dedicatedbrandScrap (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
 
@@ -14,8 +20,58 @@ async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
     console.error(e);
     process.exit(1);
   }
+
+
+}
+
+async function montlimarScrap (eshop = 'https://www.montlimart.com/99-vetements'){
+  try {
+    console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
+
+    const products = await montlimar.scrape(eshop);
+
+    console.log(products);
+    console.log('done');
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+async function circlesportsScrap (eshop = 'https://shop.circlesportswear.com/collections/collection-homme'){
+  try {
+    console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
+
+    const products = await circlesports.scrape(eshop);
+
+    console.log(products);
+    console.log('done');
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
 }
 
 const [,, eshop] = process.argv;
+circlesportsScrap(eshop);
+//circlesportsScrap(eshop);
+/*switch (true) {
+  case eshop.includes('dedicatedbrand'):
+    dedicatedbrandScrap(eshop);
+    break;
+  case eshop.includes('montlimart'):
+    montlimarScrap(eshop);
+    break;
+  case eshop.includes('circlesportswear'):
+    circlesportsScrap(eshop);
+  default:
+    console.log('no eshop found');
+    process.exit(1);
+}*/
 
-sandbox(eshop);
+
+
+
+
